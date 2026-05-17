1e2d2514d0759a8391e12957f9e47ff4f83011f8
Afffefefsgsdhtshtrhsthgfshhhhhhhhhhhhhh <yuyuaidandan@gmail.com>
2026-05-17 22:21:31 +1000

    feat: implement register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,3 +1,94 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
-  return <div>Register</div>;
+
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          role: "USER",
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessage(data.message);
+      } else {
+        setMessage(data.error);
+      }
+
+    } catch (error) {
+      setMessage("Server error");
+    }
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Register</h1>
+
+      <form onSubmit={handleRegister}>
+
+        <div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">
+          Register
+        </button>
+
+      </form>
+
+      {message && <p>{message}</p>}
+
+    </div>
+  );
 }
